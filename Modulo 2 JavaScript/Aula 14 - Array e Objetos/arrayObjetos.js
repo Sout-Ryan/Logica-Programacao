8be178bcd01bf8eeee3116e89d67e4df38a5d527
Sout-Ryan <soutogase@gmail.com>
2025-09-26 10:53:41 -0300

    Update arrayObjetos.js

--- Modulo 2 JavaScript/Aula 14 - Array e Objetos/arrayObjetos.js
+++ Modulo 2 JavaScript/Aula 14 - Array e Objetos/arrayObjetos.js
@@ -3,3 +3,15 @@ let listaProdutos = [
     {nome: "Notebook", fabricante: "Acer", valor: 3000},
     {nome: "Monitor", fabricante: "LG", valor: 900},
     {nome: "Teclado", fabricante: "Redragon", valor: 200},
+    {nome: "Mouse", fabricante: "Logitech", valor: 200}
+]
+
+listaProdutos.forEach((produto) => {
+    console.log(`O ${produto.nome} da ${produto.fabricante} é ${produto.valor}`);
+    // duas `` equivale ao f no python
+    // ex: (f"aa{variavel}") enquanto no JavaScript: (`aa ${variavel}`)
+})
+
+// listar produtos acima de 1000
+let listaProdutosCaros = listaProdutos.filter(produto => produto.valor > 1000)
+console.log(listaProdutosCaros)
