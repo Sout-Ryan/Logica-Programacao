fcb76bcf943a0288d50539ba2c22da658ae6d0e5
Sout-Ryan <soutogase@gmail.com>
2025-09-26 10:53:31 -0300

    Update arrayObjetos.js

--- Modulo 2 JavaScript/Aula 14 - Array e Objetos/arrayObjetos.js
+++ Modulo 2 JavaScript/Aula 14 - Array e Objetos/arrayObjetos.js
@@ -1,3 +1,5 @@
 let listaProdutos = [
-    {nome:}
-]
+    {nome: "Computador", fabricante: "Dell", valor: 5000},
+    {nome: "Notebook", fabricante: "Acer", valor: 3000},
+    {nome: "Monitor", fabricante: "LG", valor: 900},
+    {nome: "Teclado", fabricante: "Redragon", valor: 200},
